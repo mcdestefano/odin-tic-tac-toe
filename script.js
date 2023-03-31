11744a718acda2243d20a268023c9e7eb778441a
mcdestefano <124924561+mcdestefano@users.noreply.github.com>
2023-03-31 13:43:02 -0400

    Add functions for making move by player and on gameBoard

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const gameBoard = (() => {
   // module pattern
   const board = [];
-  return { board };
+  const addMark = (symbol, index) => {
+    board[index] = symbol;
+  };
+  return { board, addMark };
 })();
 
 const player = (name, symbol) => {
   // factory function
-  return { name, symbol };
+  const makeMove = (index) => {
+    gameBoard.addMark(this.symbol, index);
+  };
+  return { name, symbol, makeMove };
 };
 
 const gameController = () => {
   // module pattern
+  return {};
 };
