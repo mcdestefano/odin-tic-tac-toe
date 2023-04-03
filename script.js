e571caea188b5231227097f2aa8cf641f6cd186c
mcdestefano <124924561+mcdestefano@users.noreply.github.com>
2023-04-03 13:25:16 -0400

    Update board array and isGameOver to us null instead of undefined

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
 const gameBoard = (() => {
   // module pattern
-  const board = [];
+  const board = [
+    // lt
+    null,
+    // ct
+    null,
+    // rt
+    null,
+    // lc
+    null,
+    // cc
+    null,
+    // rc
+    null,
+    // lb
+    null,
+    // cb
+    null,
+    // rb
+    null
+  ];
   const addMark = (symbol, index) => {
     board[index] = symbol;
   };
   const isGameOver = () => {
     return (
       // top row
-      (board[0] === board[1] && board[1] === board[2]) ||
+      (board[0] !== null && board[0] === board[1] && board[1] === board[2]) ||
       // middle row
-      (board[3] === board[4] && board[4] === board[5]) ||
+      (board[3] !== null && board[3] === board[4] && board[4] === board[5]) ||
       // bottom row
-      (board[6] === board[7] && board[7] === board[8]) ||
+      (board[6] !== null && board[6] === board[7] && board[7] === board[8]) ||
       // left column
-      (board[0] === board[3] && board[3] === board[6]) ||
+      (board[0] !== null && board[0] === board[3] && board[3] === board[6]) ||
       // center column
-      (board[1] === board[4] && board[4] === board[7]) ||
+      (board[1] !== null && board[1] === board[4] && board[4] === board[7]) ||
       // right column
-      (board[2] === board[5] && board[5] === board[8]) ||
+      (board[2] !== null && board[2] === board[5] && board[5] === board[8]) ||
       // diagonal, tl to br
-      (board[0] === board[4] && board[4] === board[8]) ||
+      (board[0] !== null && board[0] === board[4] && board[4] === board[8]) ||
       // diagonal, tr to bl)
-      (board[2] === board[4] && board[4] === board[6])
+      (board[2] !== null && board[2] === board[4] && board[4] === board[6])
     );
   };
   return { board, addMark, isGameOver };
