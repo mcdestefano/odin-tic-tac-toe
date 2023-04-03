13055a4de90165c933785ca9942650dc9d35dc93
mcdestefano <124924561+mcdestefano@users.noreply.github.com>
2023-04-03 13:01:29 -0400

    Add functionality for determining game state and switching active player

--- script.js
+++ script.js
@@ -4,7 +4,38 @@ const gameBoard = (() => {
   const addMark = (symbol, index) => {
     board[index] = symbol;
   };
-  return { board, addMark };
+  const isGameOver = () => {
+    switch (true) {
+      case board[0] === board[1] && board[1] === board[2]:
+        // top row
+        break;
+      case board[3] === board[4] && board[4] === board[5]:
+        // middle row
+        break;
+      case board[6] === board[7] && board[7] === board[8]:
+        // bottom row
+        break;
+      case board[0] === board[3] && board[3] === board[6]:
+        // left column
+        break;
+      case board[1] === board[4] && board[4] === board[7]:
+        // center column
+        break;
+      case board[2] === board[5] && board[5] === board[8]:
+        // right column
+        break;
+      case board[0] === board[4] && board[4] === board[8]:
+        // diagonal, tl to br
+        break;
+      case board[2] === board[4] && board[4] === board[6]:
+        // diagonal, tr to bl
+        break;
+      default:
+        // not a game end state
+        break;
+    }
+  };
+  return { board, addMark, isGameOver };
 })();
 
 const player = (name, symbol) => {
@@ -15,7 +46,25 @@ const player = (name, symbol) => {
   return { name, symbol, makeMove };
 };
 
-const gameController = (() => {
+const gameController = ((p1, p2) => {
   // module pattern
-  return {};
+  let activePlayer = p1;
+  const switchPlayer = () => {
+    if (activePlayer === p1) {
+      activePlayer = p2;
+    } else {
+      activePlayer = p1;
+    }
+  };
+  const displayGameState = () => {
+    // update display with new mark
+    const gameOver = gameBoard.isGameOver();
+    if (gameOver) {
+      // disable board and display winner
+    } else {
+      switchPlayer();
+      // switch active player to other player
+    }
+  };
+  return { activePlayer, displayGameState };
 })();
